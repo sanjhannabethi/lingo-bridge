ca6215477a9e01b6a731dc5b46aebae42708bb71
Sanjhanna Bethi <sanjhannabethi@gmail.com>
2025-08-21 23:14:24 +0530

    add chat route to generate Stream token

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,23 +1,31 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-    console.error("Stream API key or Secret is missing");
+  console.error("Stream API key or Secret is missing");
 }
 
 const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
 // create or update if existing user data
 export const upsertStreamUser = async (userData) => {
-    try {
-        await streamClient.upsertUsers([userData]);
-        return userData;
-    } catch (error) {
-        console.log("Error upserting Stream user: ", error);
-    }
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.log("Error upserting Stream user: ", error);
+  }
 };
 
-export const generateStreamToken = (userId) => {};
+export const generateStreamToken = (userId) => {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error in generating Stream token:", error);
+  }
+};
